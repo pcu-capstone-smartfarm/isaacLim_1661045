5de0587b1c6f75767c3fb32c32f335547c602222
isaac9711 <isaac9711@naver.com>
2022-03-29 13:54:27 +0900

    Layout Changes

--- web/Capstone/resources/views/components/layout.blade.php
+++ web/Capstone/resources/views/components/layout.blade.php
@@ -123,7 +123,7 @@
                             <li>
                                 <a href="{{route('home')}}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                                     <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                                    <span class="ml-3">현황</span>
+                                    <span class="ml-3">홈페이지</span>
                                 </a>
                             </li>
                             <li>
@@ -138,6 +138,17 @@
                                     <span class="flex-1 ml-3 whitespace-nowrap">상품</span>
                                 </a>
                             </li>
+                            @auth
+                            <li>
+                                <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700" @click.prevent="document.querySelector('#mobile-logout-form').submit()" x-data={}>
+                                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
+                                    <span class="flex-1 ml-3 whitespace-nowrap">로그아웃</span>
+                                </a>
+                                <form id="mobile-logout-form" method="POST" action="{{route('logout', ['userID'=>auth()->user()->id])}}" class="hidden">
+                                    @csrf
+                                </form>
+                            </li>
+                            @else
                             <li>
                                 <a href="{{route('login')}}" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                                     <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
@@ -150,6 +161,7 @@
                                     <span class="flex-1 ml-3 whitespace-nowrap">회원가입</span>
                                 </a>
                             </li>
+                            @endauth
                         </ul>
                     </aside>
                 </div>
