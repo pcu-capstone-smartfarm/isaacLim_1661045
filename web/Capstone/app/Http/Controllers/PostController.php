c47f90907bd58d46a051ce40ede8115c83d5daa4
isaac9711 <isaac9711@naver.com>
2022-05-02 20:32:37 +0900

    Controller Update

--- web/Capstone/app/Http/Controllers/PostController.php
+++ web/Capstone/app/Http/Controllers/PostController.php
@@ -38,31 +38,5 @@ class PostController extends Controller
         ]);
     }
 
-    public function plantRegister(Request $request, $userID)
-    {
-        if($userID != auth()->user()->id){
-            return back()->with('fail', '사용자 정보 불일치');
-        }
-        $request->merge(['userID'=> $userID])->validate([
-            'userID' => 'required|numeric',
-            'plantType'=>'required|string',
-            'plantName' => 'required|string|max:255',
-            'serialNO' => 'required|string|max:255',
-        ]);
-        $serial = Serial::where('code', $request->serialNO)->first();
-        if($serial == null)
-        {
-            return back()->with('serialfail', '시리얼 번호 불일치');
-        }
-        $plant = new Plant([
-            'user_id' => $userID,
-            'serial_id' => $serial->id,
-            'plantname' => $request->plantName,
-            'crops_code' => $request->cropsCode,
-            'device_verification_at' => null
-        ]);
-        $plant->save();
 
-        return redirect(route('userHome', ['userID'=>auth()->user()->id]))->with('success', '식물 등록 성공');
-    }
 }
